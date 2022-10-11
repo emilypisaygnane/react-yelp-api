1d72c6e2d5bc1fe4c77f9dda7234d2d8a2e59361
Emily Pisaygnane <episaygnane@me.com>
2022-10-11 12:58:10 -0700

    created state for zipcode and added handler function to onclick button

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import { fetchBusinesses } from './services/yelp';
 function App() {
   const [businesses, setBusinesses] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [zip, setZip] = useState('98682');
 
   // TODO -- add state for zip / search and add event listeners to the inputs
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetchBusinesses();
+      const data = await fetchBusinesses('./netlify/functions/yelp');
       setBusinesses(data);
       setLoading(false);
     };
@@ -20,19 +21,25 @@ function App() {
 
   // TODO -- add event for button click to handle calling fetchBusinesses with zip / search
 
+  const handleSearch = async () => {
+    const resp = await fetch ('./netlify/functions/yelp?zip=${zip}');
+    const data = await resp.json();
+    setBusinesses(data);
+  };
+
   return (
     <div className="App">
       <h1>Alchemy Restaurant Finder</h1>
       <div className="query-form">
         <div className="form-control">
           <label>Zip:</label>
-          <input type="text" placeholder="zip" />
+          <input type="text" placeholder="zip" value={zip} onChange={(e) => setZip(e.target.value)}/>
         </div>
         <div className="form-control">
           <label>Query:</label>
           <input type="text" placeholder="Search..." />
         </div>
-        <button>Search</button>
+        <button onClick={handleSearch}>Search</button>
       </div>
       {loading && <div className="loader"></div>}
       {!loading && businesses.map((b) => <RestaurantListItem key={b.id} {...b} />)}
