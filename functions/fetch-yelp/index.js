88f1d26f85cc382ecdabe3208edd92a4982933f5
Emily Pisaygnane <episaygnane@me.com>
2022-10-11 12:47:01 -0700

    wrote out fetch function

--- functions/fetch-yelp/index.js
+++ functions/fetch-yelp/index.js
@@ -4,6 +4,26 @@ require('dotenv').config({ path: `.env.development.local` });
 const handler = async (event) => {
   // add code here to fetch data from yelp API
   // be sure to include the parameters from event.queryStringParameters
+
+  try {
+    const resp = await fetch (
+      `https://api.yelp.com/v3/businesses/search?categories=restaurants`, {
+        headers: {
+          Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+        },
+      });
+    const data = await resp.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.businesses),
+    };
+  } catch (e) {
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch data' }),
+    };
+  }
 };
 
 module.exports = { handler };
